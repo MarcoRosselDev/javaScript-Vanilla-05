4806a1c3a342ddf68ae249b471386b5eebe7d363
Marco Rossel <andresmarcorossel@gmail.com>
2022-07-30 22:12:30 -0400

    the same buth used toggle

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const button = document.querySelector('.sidebar-toggle');
 const link = document.querySelector('.sidebar');
 
 button.addEventListener('click', function() {
-    if(link.classList.contains('show-sidebar')) {
-        link.classList.remove('show-sidebar')
-    } else {
-        link.classList.add('show-sidebar')
-    }
-    // link.classList.toggle('show-sidebar');
+    // if(link.classList.contains('show-sidebar')) {
+    //     link.classList.remove('show-sidebar')
+    // } else {
+    //     link.classList.add('show-sidebar')
+    // }
+    link.classList.toggle('show-sidebar');
 });
